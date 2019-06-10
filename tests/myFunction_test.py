bdb023037f33e1e90f689a780026461be02ccc52
kureishi <yumikureishi@gmail.com>
2019-06-10 16:10:30 +0900

    test

--- tests/myFunction_test.py
+++ tests/myFunction_test.py
@@ -5,3 +5,6 @@ class SumTest(unittest.TestCase):
     def test_Sum(self):
         self.assertEqual(src.Sum(1, 1), 2)
         self.assertTrue(src.Sum(1, 2) == 3)
+
+if __name__ == '__main__':
+    unittest.main()
